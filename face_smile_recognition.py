a88b913d448c7e5c3a7a8c153d98149a46e5c339
honeyimholm <honeyimholm@gmail.com>
2017-01-07 18:16:15 -0500

    version 1.1

--- face_smile_recognition.py
+++ face_smile_recognition.py
@@ -43,7 +43,8 @@ while True:
             flags=cv2.cv.CV_HAAR_SCALE_IMAGE
         )
     # ---- Draw a rectangle around the faces
-
+    if len(faces)==0:
+            cv2.imshow('Halo2', main_img)
     for (x_1, y_1, w_1, h_1) in faces:
         cv2.rectangle(frame, (x_1, y_1), (x_1+w_1, y_1+h_1), (0, 0, 255), 2)
         roi_gray = gray[y_1:y_1+h_1, x_1:x_1+w_1]
@@ -79,7 +80,7 @@ while True:
         #halo resizing .
         #must reset halo every time
         halo_image = Image.open("Desktop\magic_mirror\halo_flat.png")
-        basewidth = 5*w_1 #resize halo to the width of the face
+        basewidth = 6*w_1 #resize halo to the width of the face
         wpercent = (basewidth / float(halo_image.size[0]))
         #print "wpercent: " + str(wpercent)
         #print "halo_image_size: " + str(halo_image.size[1])
@@ -89,7 +90,8 @@ while True:
             hsize=1
         #print "hsize: " + str(hsize)
         halo_image = halo_image.resize((basewidth, hsize))
-        halo_image.save(r'Desktop\magic_mirror\resized_halo.png')
+        #for debugging:
+        #halo_image.save(r'Desktop\magic_mirror\resized_halo.png')
         #color ordering error due to cv2 vs matplotlib pixel ordering  
         halo = np.array(halo_image)
         halo = cv2.blur(halo, (5,5))
@@ -97,29 +99,36 @@ while True:
         #print main_img.shape
         #blur_halo = cv2.GaussianBlur(halo, (5,5),0)
         #---------------------
+        #smile detection phase
         for (x, y, w, h) in smile:
             print "Found", len(smile), "smiles!"
-            thickness = 2;
-            lineType = 8;
-            cv2.rectangle(roi_color, (x, y), (x+w, y+h), (255, 0, 0), 1)
-            #cv2.ellipse(img, center, axes, angle, startAngle, endAngle, color[, thickness[, lineType[, shift]]])
-            #TODO: make the size of the ellipse correspond to the size of the head box
-            if height<0: height=0
-            cv2.ellipse(frame,(width,height),(int(w_1/2),int(w_1/6)),0,0,360,(255,255,255),7)
+        #    thickness = 2;
+        #    lineType = 8;
+        #    cv2.rectangle(roi_color, (x, y), (x+w, y+h), (255, 0, 0), 1)
+        #    #cv2.ellipse(img, center, axes, angle, startAngle, endAngle, color[, thickness[, lineType[, shift]]])
+        #    #TODO: make the size of the ellipse correspond to the size of the head box
+        #    if height<0: height=0
+        #    cv2.ellipse(frame,(width,height),(int(w_1/2),int(w_1/6)),0,0,360,(255,255,255),7)
             #TODO: resize the halo to match reflection width 
             #140x289x4
-            cv2.ellipse(main_img,(width,height),(int(w_1/2),int(w_1/6)),0,0,360,(255,255,255),7)
+        #    cv2.ellipse(main_img,(width,height),(int(w_1/2),int(w_1/6)),0,0,360,(255,255,255),7)
             #TODO: function that overlays image with existing black numpy array -> scale starting position from (width,height)
             #let's only find one smile
-            break
+        #    break
+        #-------------------------
+        #TODO: clean code
+        #TODO: add smoothing to halo repositioning
         #cv2.cv.Flip(frame, None, 1)
-        cv2.imshow('Smile Detector', frame)
+        #UNDO for debugging
+        #cv2.imshow('Smile Detector', frame)
         #halo.resize()
         #main_img = halo+main_img
+            
+            break
         c_offset = int(float(x_1+30)/float(420)*1920)
         r_offset = int(float(y_1-h_1-30)/float(240)*1080)
-        print "r_offset: " + str(r_offset)
-        print "c_offset: " + str(c_offset)
+        #print "r_offset: " + str(r_offset)
+        #print "c_offset: " + str(c_offset)#
         r_extend = (1080-halo.shape[0])-r_offset
         c_extend = (1920-halo.shape[1])-c_offset
         #deals with negative values while mainting 1920x1080 dimensionality total
@@ -136,8 +145,8 @@ while True:
         if c_offset<0:
             c_extend = c_extend-c_offset
             c_offset=1
-        print "c_extend: " + str(c_extend)
-        print "r_extend: " + str(r_extend)
+        #print "c_extend: " + str(c_extend)
+        #print "r_extend: " + str(r_extend)
         #cv2.imshow('Halo', halo)
         #we switch the c_extend and offset to account for the flipping of the camera feed
         halo = np.pad(halo, ((r_offset,r_extend),(c_extend,c_offset),(0,0)), mode='constant', constant_values=0)
